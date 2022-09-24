5489ba5760cd1ec1159f0be017a48d2873c44fd5
Kevin More <74431425+kevinmore26@users.noreply.github.com>
2022-09-23 22:04:34 -0500

    s

--- config/config.js
+++ config/config.js
@@ -6,10 +6,10 @@ const DB_NAME = process.env.DB_NAME || 'llegoshopmobile' ;
 
 
 const db = mysql.createConnection({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect(function(err) {
