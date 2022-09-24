da3840890160338417d8eef736072829a94220b3
Kevin More <74431425+kevinmore26@users.noreply.github.com>
2022-09-23 21:40:12 -0500

    commit

--- config/config.js
+++ config/config.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql');
-
+const DB_HOST = process.env.DB_HOST1 || 'localhost';
+const DB_USER = process.env.DB_USER || 'root';
+const DB_PASSWORD = process.env.DB_PASSWORD || 'password';
+const DB_NAME = process.env.DB_NAME || 'llegoshopmobile' ;
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'llegoshopmobile'
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME
 });
 
 db.connect(function(err) {
---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const logger = require('morgan');
+const cors = require('cors');
+const passport = require('passport');
+const multer = require('multer');
+const io = require('socket.io')(server);
+const mercadopago = require('mercadopago');
+mercadopago.configure({
+    sandbox: true,
+    access_token: 'TEST-1679191337926061-090115-bf2f9f2d50d79349137717ec82dee068-1190471550'
+});
+
+/*
+* IMPORTAR SOCKETS
+*/
+const ordersSocket = require('./sockets/ordersSocket');
+
+/*
+* IMPORTAR RUTAS
+*/
+const usersRoutes = require('./routes/userRoutes');
+const categoriesRoutes = require('./routes/categoryRoutes');
+const productRoutes = require('./routes/productRoutes');
+const addressRoutes = require('./routes/addressRoutes');
+const ordersRoutes = require('./routes/orderRoutes');
+const mercadoPagoRoutes = require('./routes/mercadoPagoRoutes');
+const DB_PORT = process.env.DB_PORT || 4000
+ 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+
+app.use(cors());
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.disable('x-powered-by');
+
+app.set('port', DB_PORT);
+
+/*
+* LLAMADO A LOS SOCKETS
+*/
+ordersSocket(io);
+
+const upload = multer({
+    storage: multer.memoryStorage()
+});
+
+/*
+* LLAMADO DE LAS RUTAS
+*/
+usersRoutes(app, upload);
+categoriesRoutes(app);
+addressRoutes(app);
+productRoutes(app, upload);
+ordersRoutes(app);
+mercadoPagoRoutes(app);
+
+
+server.listen(DB_PORT, '192.168.0.7' || 'localhost', function() {
+    console.log('Aplicacion de NodeJS ' + DB_PORT + ' Iniciada...')
+});
+
+
+// ERROR HANDLER
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.status || 500).send(err.stack);
+});
+
+app.get('/',  (req, res) => {
+    res.send('Ruta raiz del backend');
+});
+
+
+module.exports = {
+    app: app,
+    server: server
+}
+
+// 200 - ES UN RESPUESTA EXITOSA
+// 404 - SIGNIFICA QUE LA URL NO EXISTE
+// 500 - ERROR INTERNO DEL SERVIDOR
